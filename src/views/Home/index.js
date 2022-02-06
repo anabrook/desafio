27742d7245119c78a74ba71f7967ecaf6e556a77
Ana <anaribeirodeveloper@gmail.com>
2022-02-06 00:14:35 -0300

    adição do formulário ao projeto

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,14 +1,75 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Grid, Typography, ButtonGroup } from "@mui/material";
 
-import { Title, MainDiv, Input, OkButton, ClearButton } from "./styles";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+import {
+  Title,
+  MainDiv,
+  Input,
+  OkButton,
+  ClearButton,
+  GroupButton,
+  RowDiv,
+} from "./styles";
+
+const validationSchema = yup.object({
+  tipoIndexacao: yup.string().required("Selecione um tipo de indexação"),
+  tipoRendimento: yup.string().required("Selecione um tipo de rendimento"),
+  aporteInicial: yup.number().required("O aporte inicial é obrigatório"),
+  aporteMensal: yup.number().required("O aporte deve ser um número"),
+  prazo: yup.number().required("O prazo é obrigatório"),
+  rentabilidade: yup.number().required("A rentabilidade é obrigatória"),
+  ipa: yup.number().required("IPA é obrigatório"),
+  cdi: yup.number().required("CDI é obrigatório"),
+});
 
 function Home() {
+  const formik = useFormik({
+    initialValues: {
+      tipoIndexacao: "",
+      tipoRendimento: "",
+      aporteInicial: undefined,
+      aporteMensal: undefined,
+      prazo: undefined,
+      rentabilidade: undefined,
+      ipa: undefined,
+      cdi: undefined,
+    },
+    validationSchema: validationSchema,
+  });
+
+  const verificaBtnIndex = (value) => {
+    if (value === formik.values.tipoIndexacao) {
+      return true;
+    }
+    return false;
+  };
+
+  const verificaBtnRendi = (value) => {
+    if (value === formik.values.tipoRendimento) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <MainDiv>
-      <Grid container justifyContent="center" alignItems="center" spacing={5}>
+      <Grid container justifyContent="center" alignItems="center" spacing={3}>
         <Grid item xs={12}>
           <Title variant="h1">Simulador de investimento</Title>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography
+            variant="h6"
+            gutterBottom
+            component="div"
+            align="left"
+            fontWeight={"bold"}
+          >
+            Simulador
+          </Typography>
         </Grid>
         <Grid item xs={12}>
           <Grid
@@ -28,15 +89,85 @@ function Home() {
                 spacing={2}
               >
                 <Grid item xs={6}>
-                  <Grid container spacing={5}>
+                  <Grid container spacing={3}>
                     <Grid item xs={12}>
-                      <Input label="Aporte inicial" />
+                      <Typography
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        Rendimento
+                      </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                      <Input label="Prazo (em meses)" />
+                      <RowDiv>
+                        <ButtonGroup label="Rendimento">
+                          <GroupButton
+                            textTransformNone
+                            onClick={() =>
+                              formik.setFieldValue("tipoRendimento", "bruto")
+                            }
+                            selected={verificaBtnRendi("bruto")}
+                          >
+                            Bruto
+                          </GroupButton>
+                          <GroupButton
+                            textTransformNone
+                            onClick={() =>
+                              formik.setFieldValue("tipoRendimento", "liquido")
+                            }
+                            selected={verificaBtnRendi("liquido")}
+                          >
+                            Líquido
+                          </GroupButton>
+                        </ButtonGroup>
+                        {formik.errors.tipoRendimento && (
+                          <Typography color={"#d32f2f"} variant="caption">
+                            {formik.errors.tipoRendimento}
+                          </Typography>
+                        )}
+                      </RowDiv>
                     </Grid>
                     <Grid item xs={12}>
-                      <Input label="IPA (ao ano)" />
+                      <Input
+                        label="Aporte inicial"
+                        id="aporteInicial"
+                        name="aporteInicial"
+                        value={formik.values.aporteInicial}
+                        onChange={formik.handleChange}
+                        error={
+                          formik.touched.aporteInicial &&
+                          Boolean(formik.errors.aporteInicial)
+                        }
+                        helperText={
+                          formik.touched.aporteInicial &&
+                          formik.errors.aporteInicial
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Input
+                        label="Prazo (em meses)"
+                        id="prazo"
+                        name="prazo"
+                        value={formik.values.prazo}
+                        onChange={formik.handleChange}
+                        error={
+                          formik.touched.prazo && Boolean(formik.errors.prazo)
+                        }
+                        helperText={formik.touched.prazo && formik.errors.prazo}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Input
+                        label="IPA (ao ano)"
+                        id="ipa"
+                        name="ipa"
+                        value={formik.values.ipa}
+                        onChange={formik.handleChange}
+                        error={formik.touched.ipa && Boolean(formik.errors.ipa)}
+                        helperText={formik.touched.ipa && formik.errors.ipa}
+                      />
                     </Grid>
                     <Grid item xs={12}>
                       <ClearButton>Limpar campos</ClearButton>
@@ -44,18 +175,104 @@ function Home() {
                   </Grid>
                 </Grid>
                 <Grid item xs={6}>
-                  <Grid container spacing={5}>
+                  <Grid container spacing={3}>
                     <Grid item xs={12}>
-                      <Input label="Aporte mensal" />
+                      <Typography
+                        variant="subtitle1"
+                        gutterBottom
+                        component="div"
+                      >
+                        Tipos de indexação
+                      </Typography>
                     </Grid>
                     <Grid item xs={12}>
-                      <Input label="Rentabilidade" />
+                      <RowDiv>
+                        <ButtonGroup>
+                          <GroupButton
+                            onClick={() =>
+                              formik.setFieldValue("tipoIndexacao", "pre")
+                            }
+                            selected={verificaBtnIndex("pre")}
+                          >
+                            PRÉ
+                          </GroupButton>
+                          <GroupButton
+                            onClick={() =>
+                              formik.setFieldValue("tipoIndexacao", "pos")
+                            }
+                            selected={verificaBtnIndex("pos")}
+                          >
+                            POS
+                          </GroupButton>
+                          <GroupButton
+                            onClick={() =>
+                              formik.setFieldValue("tipoIndexacao", "fixado")
+                            }
+                            selected={verificaBtnIndex("fixado")}
+                          >
+                            FIXADO
+                          </GroupButton>
+                        </ButtonGroup>
+                        {formik.errors.tipoIndexacao && (
+                          <Typography color={"#d32f2f"} variant="caption">
+                            {formik.errors.tipoIndexacao}
+                          </Typography>
+                        )}
+                      </RowDiv>
                     </Grid>
                     <Grid item xs={12}>
-                      <Input fullWidth label="CDI (ao ano)" />
+                      <Input
+                        label="Aporte mensal"
+                        type="number"
+                        id="aporteMensal"
+                        name="aporteMensal"
+                        value={formik.values.aporteMensal}
+                        onChange={formik.handleChange}
+                        error={
+                          formik.touched.aporteMensal &&
+                          Boolean(formik.errors.aporteMensal)
+                        }
+                        helperText={
+                          formik.touched.aporteMensal &&
+                          formik.errors.aporteMensal
+                        }
+                      />
                     </Grid>
                     <Grid item xs={12}>
-                      <OkButton>Simular</OkButton>
+                      <Input
+                        label="Rentabilidade"
+                        id="rentabilidade"
+                        name="rentabilidade"
+                        type="number"
+                        value={formik.values.rentabilidade}
+                        onChange={formik.handleChange}
+                        error={
+                          formik.touched.rentabilidade &&
+                          Boolean(formik.errors.rentabilidade)
+                        }
+                        helperText={
+                          formik.touched.rentabilidade &&
+                          formik.errors.rentabilidade
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Input
+                        fullWidth
+                        label="CDI (ao ano)"
+                        id="cdi"
+                        name="cdi"
+                        type="number"
+                        value={formik.values.cdi}
+                        onChange={formik.handleChange}
+                        error={formik.touched.cdi && Boolean(formik.errors.cdi)}
+                        helperText={formik.touched.cdi && formik.errors.cdi}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <OkButton onClick={() => formik.handleSubmit()}>
+                        Simular
+                      </OkButton>
                     </Grid>
                   </Grid>
                 </Grid>
--- src/views/Home/styles.js
+++ src/views/Home/styles.js
@@ -3,7 +3,8 @@ import { styled } from "@mui/material/styles";
 import styledComponents from "styled-components";
 
 export const MainDiv = styledComponents(Box)`
-margin:20;
+margin-left:50px;
+margin-right:50px;
 height: 100vh;
 display: flex;
 align-items:center;
@@ -11,8 +12,9 @@ justify-content: center;`;
 
 export const Title = styled(Typography)(({ theme }) => ({
   ...theme.typography.h1,
-  fontSize: 28,
+  fontSize: 30,
   textAlign: "center",
+  fontWeight: "bold",
 }));
 
 export const Input = styledComponents(TextField).attrs({
@@ -27,7 +29,7 @@ export const OkButton = styledComponents(Button).attrs({
 text-transform: none;
 font-weight: bold;
 border-radius:8;
-padding:10;
+padding:10px;
 &:hover {
   color: #eee;
   background-color: #4AB377;
@@ -41,7 +43,26 @@ export const ClearButton = styledComponents(Button).attrs({
 text-transform: none;
 font-weight: bold;
 border-radius:8;
-padding:10;
+padding:10px;
 border-color: #000;
 color: #000;
+`;
+
+export const GroupButton = styledComponents(Button).attrs({
+  variant: "outlined",
+  fullWidth: true,
+})`
+font-weight: bold;
+padding:10px;
+
+&&& {
+  ${(props) =>
+    props.selected ? "background-color:#ED8E53; color: white;" : ""}
+  ${(props) => (props.textTransformNone ? "text-transform:none;" : "")}
+}
+`;
+
+export const RowDiv = styledComponents.div`
+  display: flex;
+  flex-direction: column;
 `;
